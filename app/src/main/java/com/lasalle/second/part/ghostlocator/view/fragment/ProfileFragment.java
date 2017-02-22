5b5c3602f1d6be64e42b9b913afa344d7bca5ee0
albert.denova <albert.denova@king.com>
2017-02-22 09:37:31 +0100

    Basic facebook login implementation

--- app/src/main/java/com/lasalle/second/part/ghostlocator/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/lasalle/second/part/ghostlocator/view/fragment/ProfileFragment.java
@@ -2,13 +2,23 @@ package com.lasalle.second.part.ghostlocator.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.telecom.Call;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.lasalle.second.part.ghostlocator.R;
 
 public class ProfileFragment extends Fragment {
+
+    private CallbackManager callbackManager;
 
     public static ProfileFragment newInstance() {
         ProfileFragment fragment = new ProfileFragment();
@@ -18,12 +28,42 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        callbackManager = CallbackManager.Factory.create();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        LoginButton loginButton = (LoginButton) view.findViewById(R.id.fragment_profile_facebook_login_button);
+        loginButton.setReadPermissions("email");
+        // If using in a fragment
+        loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                AccessToken accessToken = loginResult.getAccessToken();
+                Log.d("Facebook", accessToken.toString());
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
+        return view;
     }
 }
